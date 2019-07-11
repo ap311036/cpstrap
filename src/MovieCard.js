ed025a6c139f86297671c2d9fd93afea7e4616a6
S <ap311036@gmail.com>
2019-07-11 15:39:08 +0800

    change version

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,16 +1,28 @@
 import React from "react";
+import { ExclusiveLabel } from "cpstrap";
 
 const MovieCard = ({src, type}) => {
   return (
     <div className="movie-card">
       <div className="movie-card-inner">
+        <img
+          className="movie-card-placeholder"
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARAAAACZAQMAAAGqC8dkAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAABxJREFUWMPtwTEBAAAAwqD1T20MH6AAAAAAAPgbFa4AARwCsQoAAAAASUVORK5CYII="
+          alt="placeholder"
+        />
         <div className="movie-card-image-placeholder" />
         <img
+          className="movie-card-image"
           src="https://dkfhw9rzsr80z.cloudfront.net/PTS-TW-D0001-01-0001/artworks/posters/PTS-TW-D0001-01-0001-E272.jpg"
           alt="poster"
         />
       </div>
-      <div className="movie-card-zoom">zoom</div>
+      <div className="movie-card-content">
+        1.第一集
+      </div>
+      <div className="movie-card-zoom" style={{ display: "none" }}>
+        zoom
+      </div>
     </div>
   );
 };
