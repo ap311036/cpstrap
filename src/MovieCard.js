d6e807751c845c0b888c6c1ff5093349e0effd2e
S <ap311036@gmail.com>
2019-07-11 16:44:35 +0800

    chore(release): adding 1.0.23

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,7 +3,25 @@ import React from "react";
 const MovieCard = ({src, type}) => {
   return (
     <div className="movie-card">
-    card
+      <div className="movie-card-inner">
+        <img
+          className="movie-card-placeholder"
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARAAAACZAQMAAAGqC8dkAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAABxJREFUWMPtwTEBAAAAwqD1T20MH6AAAAAAAPgbFa4AARwCsQoAAAAASUVORK5CYII="
+          alt="placeholder"
+        />
+        <div className="movie-card-image-placeholder" />
+        <img
+          className="movie-card-image"
+          src="https://dkfhw9rzsr80z.cloudfront.net/PTS-TW-D0001-01-0001/artworks/posters/PTS-TW-D0001-01-0001-E272.jpg"
+          alt="poster"
+        />
+      </div>
+      <div className="movie-card-content">
+        1.第一集
+      </div>
+      <div className="movie-card-zoom" style={{ display: "none" }}>
+        zoom
+      </div>
     </div>
   );
 };
@@ -13,23 +31,3 @@ MovieCard.defaultProps = {
 };
 
 export default MovieCard;
-
-      // <div className="movie-card-inner">
-      //   <img
-      //     className="movie-card-placeholder"
-      //     src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARAAAACZAQMAAAGqC8dkAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAABxJREFUWMPtwTEBAAAAwqD1T20MH6AAAAAAAPgbFa4AARwCsQoAAAAASUVORK5CYII="
-      //     alt="placeholder"
-      //   />
-      //   <div className="movie-card-image-placeholder" />
-      //   <img
-      //     className="movie-card-image"
-      //     src="https://dkfhw9rzsr80z.cloudfront.net/PTS-TW-D0001-01-0001/artworks/posters/PTS-TW-D0001-01-0001-E272.jpg"
-      //     alt="poster"
-      //   />
-      // </div>
-      // <div className="movie-card-content">
-      //   1.第一集
-      // </div>
-      // <div className="movie-card-zoom" style={{ display: "none" }}>
-      //   zoom
-      // </div>
